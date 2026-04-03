d089b7c8bf5b2eb3702f2164109038585741af31
Puttipong Punyaporn <puttipongpunyaporn@Puttipongs-MacBook-Air.local>
2026-04-04 02:41:04 +0700

    feat: setup react router, login forms and auth context

---
+++ frontend/src/LoginForm.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { useAuth } from "./context/AuthContext.jsx";
+import { useNavigate } from 'react-router-dom';
+import './App.css';
+
+function LoginForm({loginUrl}) {
+  const { login, username: loggedInUsername } = useAuth();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch(loginUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: username, password: password }),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        login(username, data.access_token);
+        navigate('/');
+      } else if (response.status === 401) {
+        setErrorMessage("Invalid username or password");
+      }
+    } catch (error) {
+      console.log("Error logging in:", error);
+    }
+  }
+
+  return (
+    <form onSubmit={(e) => {handleLogin(e)}}>
+      Username:
+      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <br/>
+      Password:
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <br/>
+      <button type="submit">Login</button>
+      {errorMessage && <p>{errorMessage}</p>}
+      {loggedInUsername && <p>User {loggedInUsername} is already logged in.</p>}
+    </form>
+  );
+}
+
+export default LoginForm;
